8e509e209415904fdc55b1b832e7223e455b02c3
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-24 11:52:31 +0100

    styling in index naar style.php

--- index.php
+++ index.php
@@ -13,24 +13,6 @@
             <p class="search">search:<input type="search"></p>
             
     </div>
-
-    <style>
-        .body {
-            margin-top: 50px;
-            margin-left: 300px;
-            background-color: #BABABA;
-            width: 900px;
-            height: 600px;
-        }
-        .search {
-            display: flex;
-            font-size: 16px;
-            font-size: x-large;
-            justify-content: center;
-            background-color: gray;
-            padding: 18px;
-        }
-    </style>
 </body>
 
 </html>
