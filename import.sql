0ec313b58b781760e9190432c013c66931c496ad
stijn leegwater <stijn.leegwater2005@gmail.com>
2023-03-24 12:03:12 +0100

    Create import.sql

---
+++ import.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS `codeverse`;
+
+CREATE DATABASE `CodeVerse`;
+
+USE `codeverse`;
+
+CREATE TABLE `login` (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NULL,
+);
+
