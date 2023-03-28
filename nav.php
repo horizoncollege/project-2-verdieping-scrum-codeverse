f8d5c609b7032cbfcbf8596187a2a9ad32c9dbf6
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-28 11:38:54 +0200

    buttons werken nu bij de navbar

--- nav.php
+++ nav.php
@@ -7,15 +7,15 @@
 
 <body>
     <nav>
-        <h1>home</h1>
-        <h1>my code</h1>
+        <h1><a href="index.php">home</a></h1>
+        <h1><a href="#">my code</a></h1>
         <div class="dropdown">
             <h1><button class="dropbtn">account</button></h1>
 
             <div class="dropdown-content">
                 <a href="#">profile</a>
                 <a href="#">?</a>
-                <a href="#">logout</a>
+                <a href="logout.php">logout</a>
             </div>
         </div>
     </nav>
