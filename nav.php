9dffc9a929c5e322673e6922b16d7bbcd02453a8
stijn leegwater <stijn.leegwater2005@gmail.com>
2023-03-29 09:30:51 +0200

    navbar update

--- nav.php
+++ nav.php
@@ -8,13 +8,12 @@
 <body>
     <nav>
         <h1><a href="index.php"><input class="nav-button" type="submit" value="home"></a></h1>
-        <h1><a href="my-code.php"><input class="nav-button" type="submit" value="my code"></a></h1>
+        <h1><a href="contact.php"><input class="nav-button" type="submit" value="contact"></a></h1>
         <div class="dropdown">
             <h1><button class="dropbtn">account</button></h1>
 
             <div class="dropdown-content">
-                <a href="contact.php">contact</a>
-                <a href="#">?</a>
+                <a href="my-code.php">my codes</a>
                 <a href="logout.php">logout</a>
             </div>
         </div>
