8a8e1cc20d3037e09e368c7e9d842705fe309154
timo-kievit <170355@student.horizoncollege.nl>
2023-03-29 08:39:01 +0200

    my code page front end afgemaakt

---
+++ my-code.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>CodeVerse</title>
+</head>
+
+<body>
+    <?php include('nav.php'); ?>
+    <h1 class="my-code-title">my code</h1>
+    <div class="body">
+            <p class="search">search:<input type="search"></p>
+            
+    </div>
+</body>
+
+</html>
