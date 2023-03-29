1ea2aa3044dd41b3b99e74573fbb6fd84b2d6a59
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-29 10:37:34 +0200

    input type tekst bij de detail pagina

--- detail.php
+++ detail.php
@@ -9,15 +9,25 @@
 <body>
     <?php include('nav.php'); ?>
     <h2>detail</h2>
-    
+
     <div class="maincontainer">
-            <h3>repository name:</h3>
-            <p class="detailinh">date of upload/last update:</p>
-            <p class="detailinh">languages and license:</p>
-            <p class="detailinh">author:</p>
-            <p class="detailinh">tags:</p>
-    </div>
-    <?php include('footer.php'); ?>
+        <h3>repository name:</h3>
+        <p class="detailinh">date of upload/last update:</p>
+        <p class="detailinh">languages and license:</p>
+        <p class="detailinh">author:</p>
+        <p class="detailinh">tags:</p>
+        <h3>code:</h3>
+        <div class="edit-code-input">
+           
+            <textarea class="detailcode" rows="20" type="text"></textarea>
+        </div>
+            <?php include('footer.php'); ?>
 </body>
 
 </html>
+
+<style>
+    .detailcode {
+        width: 40rem;
+    }
+</style>
