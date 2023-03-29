04360290b3f8a99ec3c634bfee90b6dc1f79a362
timo-kievit <170355@student.horizoncollege.nl>
2023-03-29 09:11:44 +0200

    edit page update

--- edit.php
+++ edit.php
@@ -9,20 +9,23 @@
 <body>
     <?php include('nav.php'); ?>
     <h1 class="my-code-title">edit code</h1>
-    <div class="body">
-    <div class="edit-code-text">
-    <h2>repisotory name:</h2>
-    <h4>date of upload/lastupdate:</h4>
-    <h4>languages and license:</h4>
-    <h4>author:</h4>
-    <h4>tags:</h4>
-    </div>
-    <div class="edit-code-input">
-    <input type="">
-    </div>
-    <div class="edit-code-button">
+    <div class="edit-body">
+        <div class="input-box">
+            <div class="edit-code-text">
+                <h2>repisotory name:</h2>
+                <h4>date of upload/lastupdate:</h4>
+                <h4>languages and license:</h4>
+                <h4>author:</h4>
+                <h4>tags:</h4>
+            </div>
+            <h2>code: </h2>
+            <div class="edit-code-input">
+                <textarea class="edit-code-input-text" rows="35" type="text"></textarea>
+            </div>
+            <div class="edit-code-button">
 
-    </div>
+            </div>
+        </div>
     </div>
 </body>
 
