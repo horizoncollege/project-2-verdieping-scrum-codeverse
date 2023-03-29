e41b1f35dc7cbccc01d136fcf2408ebdf1887378
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-29 08:38:49 +0200

    logo  naam veranderd en goed gelinked

---
+++ detail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>CodeVerse</title>
+</head>
+
+<body>
+    <?php include('nav.php'); ?>
+
+    <div class="body">
+            <p class="search">search:<input type="search"></p>
+            
+    </div>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 
 <body>
     <div class="logincontainer">
-        <img class="logo" src="images/Schermafbeelding 2023-03-24 100129.png">
+        <img class="logo" src="images/logo.png">
         <br>
             <div class="login">
             <form action="" method="post">
