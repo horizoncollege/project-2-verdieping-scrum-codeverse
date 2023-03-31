9053fbbaebead564537b6e3e8a23714315d5f5fe
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-31 11:04:47 +0200

    style in style

--- footer.php
+++ footer.php
@@ -19,22 +19,3 @@
 </body>
 
 </html>
-
-<style>
-    .footerlogo {
-        width: 120px;
-        height: 100px;
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
-
-    footer {
-        color: #ffffff;
-        margin-top: 5%;
-        background-color: #0621B2;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        position: sticky;
-    }
-</style>
