1cf50b56b20bb27994635b649a257f1fe48d6b44
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-31 09:46:41 +0200

    begin database gemaakt

--- import.sql
+++ import.sql
@@ -1,3 +1,23 @@
 DROP DATABASE IF EXISTS `codeverse`;
 
 CREATE DATABASE `codeverse`;
+
+CREATE TABLE `code` (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Repository_name VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    Last_Update VARCHAR(100) NOT NULL,
+    Tags VARCHAR(100) NOT NULL,
+    language VARCHAR(100) NOT NULL,
+    licence VARCHAR(100) NOT NULL,
+    code TEXT NOT NULL
+);
+
+    INSERT INTO `code` (`Repository_name`, `Author`, `Last_Update`, `Tags`, `language`, `licence`, `code`) VALUES
+	('galgje','Tim Abbing', '31-03-2023', 'PHP','PHP' , 'rfbo', 'hello');
+
+
+
+
+
+
