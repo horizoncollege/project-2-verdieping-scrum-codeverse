d2b9e6b9fdb540e06780b712d76c4bceae19917d
Tim-Abbing <169822@student.horizoncollege.nl>
2023-03-31 08:43:04 +0200

    styling webiste

--- detail.php
+++ detail.php
@@ -18,10 +18,26 @@
         <p class="detailinh">tags:</p>
         <h3>code:</h3>
         <div class="edit-code-input">
-           
-            <textarea class="detailcode" rows="20" type="text"></textarea>
+            <textarea class="detailcode" rows="20" type="text" readonly></textarea>
         </div>
-            <?php include('footer.php'); ?>
+        <?php include('footer.php'); ?>
 </body>
 
 </html>
+
+<style>
+    body {
+        background-image: url('images/codebackground.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+    nav {
+    background-color: gray;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    position: sticky;
+    background-color: #2F80ED;
+}
+</style>
