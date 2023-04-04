75c9ab71b00a9c2f9008b78b97c3080d15d6188c
Tim-Abbing <169822@student.horizoncollege.nl>
2023-04-04 11:31:42 +0200

    footer style in style.css

--- nav.php
+++ nav.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
     <nav>
         <h1><a href="index.php"><input class="button-name" type="submit" value="home"></a></h1>
         <h1><a href="contact.php"><input class="button-name" type="submit" value="contact"></a></h1>
+
+        <!--here  -->
         <div class="dropdown">
             <h1><button class="button-name">account</button></h1>
-
             <div class="dropdown-content">
                 <a href="mycode.php">my code</a>
                 <a href="logout.php">logout</a>
             </div>
         </div>
     </nav>
-</body>
-
-</html>
