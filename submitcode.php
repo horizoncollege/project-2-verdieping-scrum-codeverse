020e050bef398853ba01ad76be852eee42447913
stijn leegwater <stijn.leegwater2005@gmail.com>
2023-04-04 09:11:53 +0200

    submit page updated

--- submitcode.php
+++ submitcode.php
@@ -10,16 +10,18 @@
     <?php include('nav.php'); ?>
     <h2>submit code</h2>
 
-    <div class="maincontainer">
-        <h3>repository name:</h3>
+    <div class="submitcontainer">
+        <div class="codeinfo">
+        <h3>repository name: <textarea name="name" id="name" cols="30" rows="1"></textarea></h3>
         <p class="detailinh">date of upload/last update:</p>
         <p class="detailinh">languages and license:</p>
         <p class="detailinh">author:</p>
         <p class="detailinh">tags:</p>
-        <h3>code:</h3>
+        <h3>code:</h3></div>
         <div class="edit-code-input">
             <textarea class="detailcode" rows="20" type="text"></textarea>
         </div>
+        <div class="submitbutton"></div>
         <button>submit</button>
     </div>
     <?php include('footer.php'); ?>
