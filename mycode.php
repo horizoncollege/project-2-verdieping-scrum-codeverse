8f9b003c88da1d8c579e71cebd8d92e157299d80
Frederikusvm <117975812+Frederikusvm@users.noreply.github.com>
2023-04-04 10:49:09 +0200

    Font anders

--- mycode.php
+++ mycode.php
@@ -3,16 +3,20 @@
 
 <head>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>CodeVerse</title>
 </head>
 
 <body>
     <?php include('nav.php'); ?>
-    <h1 class="my-code-title">my code</h1>
-    <div class="maincontainer">
-            <p class="search">search:<input type="search"><a href="submitcode.php"><input class="add-code-button" type="submit" value="add-code"></a></p>
 
+    <div class="maincontainer">
+        <p class="search">search:<input type="search">
+        <a href="submitcode.php"><input class="add-code-button" type="submit" value="Add code"></a></p>
     </div>
+
     <?php include('footer.php'); ?>
 </body>
 
