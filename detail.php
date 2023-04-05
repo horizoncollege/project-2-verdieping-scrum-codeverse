469c43794bee25e549d420578d5da0a4fc0f628c
timo-kievit <170355@student.horizoncollege.nl>
2023-04-05 10:34:21 +0200

    highlighting eerste update

--- detail.php
+++ detail.php
@@ -6,13 +6,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/stackoverflow-dark.min.css">
     <title>CodeVerse</title>
 </head>
 
 <body>
     <?php include('nav.php'); ?>
     <h2>detail</h2>
-    <!--the datails of the repository's-->
+ <!--the datails of the repository's-->
     <div class="maincontainer">
         <h3>repository name:</h3>
         <p class="detailinh">date of upload/last update:</p>
@@ -21,9 +22,28 @@
         <p class="detailinh">tags:</p>
         <h3>code:</h3>
         <div class="edit-code-input">
-            <textarea class="detailcode" rows="20" type="text" readonly></textarea>
+        <pre>
+        <code class="language-html">
+&lt;!DOCTYPE html&gt;
+&lt;html lang="en"&gt;
+&lt;head&gt;
+&lt;meta charset="UTF-8"&gt;
+&lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;
+&lt;meta name="viewport" content="width=device-width, initial-scale=1.0"&gt;
+&lt;title&gt;Document&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;
+
+&lt;/body&gt;
+&lt;/html&gt;
+        </code>
+    </pre>
         </div>
     </div>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
+    <script>
+        hljs.highlightAll();
+    </script>
     <?php include('footer.php'); ?>
 </body>
 </html>
