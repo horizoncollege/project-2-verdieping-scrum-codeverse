0476ba6234de7dcd9977b3c1b5c3aa072383bdf5
Tim-Abbing <169822@student.horizoncollege.nl>
2023-04-06 12:26:08 +0200

    basic design edit en submitcode

--- edit.php
+++ edit.php
@@ -6,28 +6,21 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <title>CodeVerse</title>
+    <title>CodeVerse details</title>
 </head>
 
 <body>
-    <!--here is linked to the nav.php so you can see the navbar -->
-    <?php include('nav.php'); ?>
-    <h2>edit</h2>
-    <!--input in the main container -->
+<?php include('nav.php'); ?>
+ <!--the datails of the repository's-->
     <div class="maincontainer">
-        <h3>repository name:</h3>
-        <p class="detailinh">date of upload/last update:</p>
-        <p class="detailinh">languages and license:</p>
-        <p class="detailinh">author:</p>
-        <p class="detailinh">tags:</p>
-        <h3>code:</h3>
-        <div class="edit-code-input">
-            <textarea class="detailcode" rows="20" type="text"></textarea>
-        </div>
-        <button>submit</button>
+        <h3>repository</h3>
+        <p class="detailinh">Made by:</p>
+        <p class="detailinh">Tags:</p>
+        <p class="detailinh">Last version:</p>
+        <p class="detailinh">Licence:</p>
+        <p class="detailinh">Code:</p>
+        <textarea disabled class="codefield"></textarea>
     </div>
-    <!--here is linked to the footer.php so you can see the footer -->
     <?php include('footer.php'); ?>
 </body>
-
 </html>
